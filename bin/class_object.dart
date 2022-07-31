cc0f027559e807c4580a3c4af7e4935bcde9c0ed
vishnuacappella <acappella.studiov@gmail.com>
2022-07-31 06:14:48 +0530

    dart file create

---
+++ bin/class_object.dart
@@ -0,0 +1,5 @@
+import 'package:class_object/class_object.dart' as class_object;
+
+void main(List<String> arguments) {
+  print('Hello world: ${class_object.calculate()}!');
+}
---
+++ test/class_object_test.dart
@@ -0,0 +1,8 @@
+import 'package:class_object/class_object.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
